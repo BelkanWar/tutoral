2e58883ebfd748af85bd9051e05264ed2a86e309
BelkanWar <challenger1501@gmail.com>
2024-10-19 01:43:24 +0800

    2024-10-19 progress

--- regression/logistic_regression.py
+++ regression/logistic_regression.py
@@ -6,23 +6,42 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from plotly import express
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
 spector_data = pd.DataFrame(sm.datasets.spector.load().data)
-sns.scatterplot(spector_data, x='TUCE', y='GRADE')
+sns.scatterplot(spector_data, x='GPA', y='GRADE')
 plt.show()
 
 fig = express.scatter_3d(spector_data, x='GPA', y='TUCE', z='GRADE')
 fig.show()
 
-# data shuffling
-spector_data = spector_data.sample(frac=1)
-
 # build a model using statsmodels
-model = smf.glm(formula='GRADE ~ GPA + TUCE + PSI', data=spector_data, family=sm.families.Binomial()).fit()
+model = smf.glm(formula='GRADE ~ GPA', data=spector_data, family=sm.families.Binomial()).fit()
 print(model.summary())
-prediction = model.predict(spector_data)
+
+pred_prob = model.predict(spector_data)
+spector_data['stats_prob'] = pred_prob
 
 # build a model using sci-kit learn
 model = LogisticRegression()
-model.fit(spector_data[['GPA', 'TUCE', 'PSI']], spector_data['GRADE'])
-prediction = model.predict_proba(spector_data[['GPA', 'TUCE', 'PSI']])
+model.fit(spector_data[['GPA']], spector_data['GRADE'])
+prediction = model.predict_proba(spector_data[['GPA']])
+pred_prob = prediction[:, 1]
+spector_data['ml_prob'] = pred_prob
+
+# build a naive bayes model
+model = GaussianNB()
+model.fit(spector_data[['GPA']], spector_data['GRADE'])
+prediction = model.predict_proba(spector_data[['GPA']])
+pred_prob = prediction[:, 1]
+spector_data['NB'] = pred_prob
+
+# visulization
+spector_data = spector_data.sort_values('GPA')
+fig, axis = plt.subplots(1, 1)
+axis.plot(spector_data['GPA'], spector_data['stats_prob'], color='r')
+axis.plot(spector_data['GPA'], spector_data['ml_prob'], color='g')
+axis.plot(spector_data['GPA'], spector_data['NB'], color='b')
+axis.scatter(spector_data['GPA'], spector_data['GRADE'])
+plt.show()
+plt.close()
