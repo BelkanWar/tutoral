b577b5a5515b8b6adbb61c64734f637f14d22839
samwang1 <samwang@ghtinc.com>
2024-10-18 18:42:17 +0800

    2024-10-18 preperation

--- regression/logistic_regression.py
+++ regression/logistic_regression.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from plotly import express
+from sklearn.linear_model import LogisticRegression
 
 spector_data = pd.DataFrame(sm.datasets.spector.load().data)
 sns.scatterplot(spector_data, x='TUCE', y='GRADE')
@@ -12,3 +13,16 @@ plt.show()
 
 fig = express.scatter_3d(spector_data, x='GPA', y='TUCE', z='GRADE')
 fig.show()
+
+# data shuffling
+spector_data = spector_data.sample(frac=1)
+
+# build a model using statsmodels
+model = smf.glm(formula='GRADE ~ GPA + TUCE + PSI', data=spector_data, family=sm.families.Binomial()).fit()
+print(model.summary())
+prediction = model.predict(spector_data)
+
+# build a model using sci-kit learn
+model = LogisticRegression()
+model.fit(spector_data[['GPA', 'TUCE', 'PSI']], spector_data['GRADE'])
+prediction = model.predict_proba(spector_data[['GPA', 'TUCE', 'PSI']])
