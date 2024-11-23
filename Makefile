a9758b0674f682ad8fcbe5b33395d982bd6c91a2
BelkanWar <challenger1501@gmail.com>
2024-11-23 21:59:56 +0800

    ready

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-IMAGE_NAME="db_image"
-CONTAINER_NAME="db_container"
+IMAGE_NAME="mysql_image"
+CONTAINER_NAME="mysql_container"
 
 build-image ::
 	docker build --rm -f docker_and_DB/Dockerfile -t ${IMAGE_NAME} .
 
 container-up ::
-	docker run -itd --rm --name ${CONTAINER_NAME} ${IMAGE_NAME}
+	docker run -itd --rm --name ${CONTAINER_NAME} -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v /opt/mysql/:/var/lib/mysql/ ${IMAGE_NAME}
 
 container-down ::
 	docker stop ${CONTAINER_NAME}
---
+++ docker_and_DB/DB_operates.py
@@ -0,0 +1,51 @@
+import pymysql
+from sklearn import datasets
+
+# DB connecting 
+db = pymysql.connect(host='localhost', user='root',password='123456')
+cursor = db.cursor()
+
+# create a database
+cursor.execute("CREATE DATABASE myDB")
+
+# create a table 
+cursor.execute(
+    """
+    CREATE TABLE myDB.iris (
+        id INT NOT NULL AUTO_INCREMENT, 
+        species VARCHAR(30) NOT NULL,
+        sepal_length FLOAT NOT NULL ,
+        sepal_width FLOAT NOT NULL,
+        petal_length FLOAT NOT NULL,
+        petal_width FLOAT NOT NULL,
+        timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+        PRIMARY KEY (id)
+    )
+    """
+)
+
+cursor.execute(
+    """
+    CREATE INDEX Species 
+    ON myDB.iris (species)
+    """
+)
+
+# insert some data
+iris = datasets.load_iris()
+
+for row in range(iris.data.shape[0]):
+    species = iris.target_names[iris.target[row]].tolist()
+    sepal_len, sepal_width, petal_len, petal_width = iris.data[row].tolist()
+
+    cursor.execute(
+        """
+        INSERT INTO myDB.iris (species, sepal_length, sepal_width, petal_length, petal_width)
+        VALUES (%s, %s, %s, %s, %s)
+        """, (species, sepal_len, sepal_width, petal_len, petal_width)
+    )
+    db.commit()
+
+
+# disconnect
+db.close()
